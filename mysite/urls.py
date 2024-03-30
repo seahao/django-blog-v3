a6463b0ac9c486f6018e4426b93d17274bf9d8e1
Hao <haowaer@gmail.com>
2024-03-29 22:19:32 -0700

    lesson_7 first commit v1.1

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include  # <-- Make sure you have both of these imports.
 
 urlpatterns = [
+    path('', include('blogging.urls')),
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
 ]
