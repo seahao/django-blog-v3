0cb624013081ed791d5144081dee782b94ee1f28
Hao <haowaer@gmail.com>
2024-03-31 12:22:58 -0700

    title changed

--- blogging/tests.py
+++ blogging/tests.py
@@ -52,7 +52,7 @@ class FrontEndTestCase(TestCase):
     def test_list_only_published(self):
         resp = self.client.get("/")
         resp_text = resp.content.decode(resp.charset)
-        self.assertTrue("Cool Blog Posts" in resp_text)
+        self.assertTrue("Coolest Blog Posts" in resp_text)
         for count in range(1, 11):
             title = "Post %d Title" % count
             if count < 6:
