40586db175d4b61343d1b86cc2cc26c5aa46d620
Hao <haowaer@gmail.com>
2024-03-31 10:09:13 -0700

    Lesson 8 completed

--- blogging/views.py
+++ blogging/views.py
@@ -2,7 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+
+    def get_queryset(self):
+        return Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = Post.objects.exclude(published_date__exact=None)
+        pk = self.kwargs.get('pk')
+        try:
+            post = queryset.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404("not exist or published")
+
+        return post
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
     if args:
@@ -12,23 +36,3 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
-
-# and this view
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
-
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
-
